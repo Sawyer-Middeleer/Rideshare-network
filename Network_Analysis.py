4693e00ac81afdc91ac3f4e6f5882ded296b347b
Sawyer Middeleer <saw.midd@gmail.com>
2019-11-08 10:36:06 -0600

    Update Network_Analysis.py

--- Network_Analysis.py
+++ Network_Analysis.py
@@ -1,32 +1,20 @@
-import os
-import us
 import pandas as pd
-import numpy as np
 import requests
-import networkx as nx
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 from sodapy import Socrata
 
-class ChicagoRides:
-
-    def __init__(self):
-        self.TNC_RIDES_API_ENDPOINT = 'https://data.cityofchicago.org/resource/m6dm-c72p.json'
-        self.CLIENT = Socrata("data.cityofchicago.org", None)
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+# client = Socrata("data.cityofchicago.org", None)
 
 
-    def get_rides_data(self):
-        rides = self.CLIENT.get("m6dm-c72p", limit=2000)
-        rides_df = pd.DataFrame.from_records(rides)
-        return rides_df
+client = Socrata('data.cityofchicago.org',
+                 'g7JL0nhe6EdUFQOCY3bEG0kpo',
+                  username='sawyerm@uchicago.edu',
+                  password='Seishonagon12!')
 
 
+results = client.get("m6dm-c72p", limit=2000)
 
-
-def main():
-    chi_rides = ChicagoRides()
-
-
-
-if __name__ == "__main__":
-    main()
+# Convert to pandas DataFrameC
+results_df = pd.DataFrame.from_records(results)
+results_df
